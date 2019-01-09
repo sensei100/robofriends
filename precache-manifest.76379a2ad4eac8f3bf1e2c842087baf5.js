775d04b60a2e261935479c1bdb7f03a83f9bc71b
sensei100 <tuh42082@temple.edu>
2019-01-09 13:16:47 -0500

    Updates

---
+++ precache-manifest.76379a2ad4eac8f3bf1e2c842087baf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "30dceb296c1079710646",
+    "url": "/robofriends/static/js/main.30dceb29.chunk.js"
+  },
+  {
+    "revision": "22688794e88a8cfe3b54",
+    "url": "/robofriends/static/js/1.22688794.chunk.js"
+  },
+  {
+    "revision": "30dceb296c1079710646",
+    "url": "/robofriends/static/css/main.9c8f0a17.chunk.css"
+  },
+  {
+    "revision": "22688794e88a8cfe3b54",
+    "url": "/robofriends/static/css/1.b4855ee2.chunk.css"
+  },
+  {
+    "revision": "6b31dcce2b065b66af410c77932fbc76",
+    "url": "/robofriends/index.html"
+  }
+];
