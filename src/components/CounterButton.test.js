9e062bd985dee02a642f5a3ccc657b5f7bdbbce7
sensei100 <tuh42082@temple.edu>
2019-01-14 14:38:38 -0500

    Test stateful components

---
+++ src/components/CounterButton.test.js
@@ -0,0 +1,19 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import CounterButton from './CounterButton';
+
+it('renders CounterButton component', () => {
+  const mockColor = 'red';
+  expect(shallow(< CounterButton color={mockColor} />)).toMatchSnapshot();
+})
+
+it('correctly increments the counter', () => {
+  const mockColor = 'red';
+  const wrapper = shallow(< CounterButton color={mockColor} />);
+
+  wrapper.find('[id="counter"]').simulate('click');
+  expect(wrapper.state()).toEqual({ count: 1});
+  wrapper.find('[id="counter"]').simulate('click');
+  expect(wrapper.state()).toEqual({ count: 2});
+  expect(wrapper.props().color).toEqual('red');
+})
