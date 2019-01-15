d9557886ea2cd7cf535359d5230d56638cba02b4
sensei100 <tuh42082@temple.edu>
2019-01-15 15:23:53 -0500

    Test connected components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
-import Header from '../components/Header';
+import MainPage from '../components/MainPage';
 import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
@@ -25,28 +21,8 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 class App extends Component {
-  componentDidMount() {
-    this.props.onRequestRobots();
-  }
-
   render() {
-    const { searchField, onSearchChange, robots, isPending } = this.props;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase())
-    })
-    return isPending ?
-      <h1>Loading...</h1> :
-      (
-      <div className='tc'>
-      <Header />
-        <SearchBox searchChange={onSearchChange} />
-        <Scroll>
-          <ErrorBoundary>
-            <CardList robots={filteredRobots} />
-          </ErrorBoundary>
-        </Scroll>
-      </div>
-      );
+    return <MainPage { ...this.props }/>
   }
 }
 
